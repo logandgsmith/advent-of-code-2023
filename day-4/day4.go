8ee84b97543f80e0d60c66398391aa2784c8280d
Logan Smith <logandawsonsmith@gmail.com>
2023-12-04 21:55:52 -0500

    Day 4 Part 1 completed

--- day-4/day4.go
+++ day-4/day4.go
@@ -5,8 +5,12 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math"
 	"os"
+	"strings"
 )
+
+
 
 func main() {
 	filePath := flag.String("fpath", "./input.txt", "File path to be read from")
@@ -22,13 +26,46 @@ func main() {
 			log.Fatal(err)
 		}
 	}()
+
+	totalValue := 0;
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		// Split game data
+		initialSplit := strings.Split(scanner.Text(), ":")
+		//cardName := initialSplit[0]
+		cardsString := initialSplit[1]
+
+		// Split hands
+		cardSplit := strings.Split(cardsString, "|")
+		winningNumbers := strings.Split(cardSplit[0], " ")
+		chosenNumbers := strings.Fields(cardSplit[1])
+
+		//fmt.Println(len(chosenNumbers))
+
+		matches := 0
+		for _, number := range chosenNumbers {
+			for _, winningNumber := range winningNumbers {
+				if number == winningNumber {
+					//fmt.Println(number)
+					matches++
+					continue
+				}
+			}
+		}
+		//fmt.Println(matches)
+
+		if matches > 0 {
+			cardPoints := int(math.Pow(2.0, float64(matches - 1)))
+			//fmt.Println(cardPoints)
+			totalValue += cardPoints
+		}
 	}
 	err = scanner.Err()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Printf("Total score:%d\n", totalValue)
 
 }
